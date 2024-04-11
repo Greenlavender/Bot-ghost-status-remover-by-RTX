ed74c51e1e569448b775a82d9d890c43bbec3582
Greenlavender <163481453+Greenlavender@users.noreply.github.com>
2024-04-10 19:07:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["🔗 https://discord.gg/YXuzjrpDbH"];
 
 
 let currentIndex = 0;
